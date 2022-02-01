d2c1707a8ff78a5e829f7e83b169c2f8ed21641f
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-02-01 18:33:28 +0530

    Update Ascendingorder.java

--- Ascendingorder.java
+++ Ascendingorder.java
@@ -4,18 +4,39 @@ public class Ascendingorder
 {
 	public static void main(String[] args) 
 	{
-		
-		int numbers[] = new int[]{12,53,33,88,13};
-		int largenumber = numbers[0];
-		for(int i=1; i< numbers.length; i++)
-		{
-		if(numbers[i] > largenumber)
-		largenumber = numbers[i];
-		
-		}
-		System.out.println("Largest Number is : " +
-		largenumber);
-		
-		}
+		 int [] arr = new int [] {8, 1,6,9,2};     
+        int temp = 0;    
+            
+        
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr.length; i++)
+	{     
+            System.out.print(arr[i] + " ");    
+        }    
+            
+          
+        for (int i = 0; i < arr.length; i++) 
+	
+	{     
+            for (int j = i+1; j < arr.length; j++)
+	    {     
+               if(arr[i] > arr[j])
+	       {    
+                   temp = arr[i];    
+                   arr[i] = arr[j];    
+                   arr[j] = temp;    
+               }     
+            }     
+        }    
+          
+        System.out.println();    
+            
+       
+        System.out.println("Elements of array sorted in ascending order: ");    
+        for (int i = 0; i < arr.length; i++)
+	{     
+            System.out.print(arr[i] + " ");    
+        }    
+    
 
 }
